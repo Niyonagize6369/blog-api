19f4778567c51442b489c08e00bb356a05bd0da8
Your Name <niyonagizerachel10@email.com>
2025-06-02 12:41:21 +0200

    Initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,34 @@
 import 'reflect-metadata';
 import express, { Express } from 'express';
 import * as dotenv from 'dotenv';
-import authRoutes from './routes/auth.routes';
-
-import usersRoutes from './routes/users.routes';
-import postRoutes from './routes/blog.routes';
 import { initializeDatabase } from './config/database';
 import { errorHandler } from './middleware/errorHandler';
 
+import routes from './routes/index';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger-output.json';
+
+const app = express();
+
+// app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(express.json({ limit: '16kb' }));
+app.use(express.urlencoded({ extended: true, limit: '16kb' }));
+app.use(express.static('public'));
+
+// Mount API routes under /api/v1 prefix
+app.use('/api/v1', routes);
+
+// Serve Swagger documentation
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
 dotenv.config();
 
-const app: Express = express();
 const PORT: number = parseInt(process.env.PORT || '5000');
 
 //Middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-
-// Routes
-
-app.use('/auth', authRoutes);
-app.use('/users', usersRoutes);
-app.use('/post', postRoutes);
-
 
 // Error handling middleware
 app.use(errorHandler);
